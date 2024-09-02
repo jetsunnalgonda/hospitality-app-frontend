1bc4839324c688be2b55331f78aa3bf66717c819
Haluk Isik <isikhaluk@gmail.com>
2024-09-02 15:56:16 +0300

    updates

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: 'all', // Allow all hosts
+    // disableHostCheck: true,
+    // OR if you prefer to only allow specific hosts, use:
+    // allowedHosts: ['Jetsuns-MacBook-Air.local', 'localhost'],
+  }
 })
