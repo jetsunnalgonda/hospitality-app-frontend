42d9dd0fd97aa2850d8352fce71f8e63a265830e
Haluk Isik <isikhaluk@gmail.com>
2024-09-03 00:41:53 +0300

    optimized apiClient.js

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -6,6 +6,7 @@ const apiClient = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL,
 });
 
+// Request interceptor to add token to headers
 apiClient.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
@@ -14,45 +15,13 @@ apiClient.interceptors.request.use(config => {
   return config;
 });
 
+// Response interceptor to handle token expiration
 apiClient.interceptors.response.use(
   response => response,
   async error => {
     const originalRequest = error.config;
 
-    if (error.response && error.response.status === 401 && !originalRequest._retry) {
-      // Handle token refresh on a 401 error
-      originalRequest._retry = true;
-
-      try {
-        // Call Vuex action to refresh the token
-        await store.dispatch('refreshToken');
-
-        // Retry the original request with the new token
-        const token = localStorage.getItem('token'); // Get updated token
-        originalRequest.headers.Authorization = `Bearer ${token}`;
-        return apiClient(originalRequest);
-      } catch (refreshError) {
-        // Clear tokens and redirect to login if refresh fails
-        localStorage.removeItem('token');
-        localStorage.removeItem('refreshToken');
-        await store.dispatch('logout'); // Dispatch Vuex logout action
-        router.push('/login');
-        alert('Your session has expired. Please log in again.');
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
-
-apiClient.interceptors.response.use(
-  response => response,
-  async error => {
-    const originalRequest = error.config;
-
-    console.log('error.response.status = ' + error.response.status);
-
-    if (error.response && error.response.status === 403 && !originalRequest._retry) {
+    if (error.response && (error.response.status === 401 || error.response.status === 403) && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
@@ -62,14 +31,10 @@ apiClient.interceptors.response.use(
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('refreshToken', response.data.refreshToken);
 
-        console.log('got the new tokens')
-        console.log('token: ' + response.data.token)
-        console.log('refresh token: ' + response.data.refreshToken)
-
         originalRequest.headers.Authorization = `Bearer ${response.data.token}`;
         return apiClient(originalRequest);
       } catch (refreshError) {
-        store.dispatch('logout');
+        await store.dispatch('logout');
         router.push('/login');
         alert('Your session has expired. Please log in again.');
       }
